5f21944b710ca33476a2abba3be0010a4eb27ecb
ymdysk <ymdysk@sic.shimano.co.jp>
2022-09-30 18:57:24 +0900

    Get image from strage

--- app/src/main/java/jp/techacademy/yusuke/autoslideshowapp/MainActivity.kt
+++ app/src/main/java/jp/techacademy/yusuke/autoslideshowapp/MainActivity.kt
@@ -1,25 +1,64 @@
 package jp.techacademy.yusuke.autoslideshowapp
 
+import android.Manifest
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.content.pm.PackageManager
+import android.os.Build
+import android.util.Log
+import android.provider.MediaStore
+import android.content.ContentUris
 import android.os.Handler
 import kotlinx.android.synthetic.main.activity_main.*
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
+    private val PERMISSIONS_REQUEST_CODE = 100
     private var mTimer: Timer? = null
 
     // タイマー用の時間のための変数
     private var mTimerSec = 0.0
-
     private var mHandler = Handler()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        start_button.setOnClickListener {
+        // 外部ストレージへのアクセス許可取得
+        // Android 6.0以降の場合
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            // パーミッションの許可状態を確認する
+            if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+                // 許可されている
+                //getContentsInfo()
+            } else {
+                // 許可されていないので許可ダイアログを表示する
+                requestPermissions(
+                    arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),
+                    PERMISSIONS_REQUEST_CODE
+                )
+            }
+            // Android 5系以下の場合
+        } else {
+            //getContentsInfo()
+        }
+
+        if (mTimer == null) {
+            mTimer = Timer()
+            mTimer!!.schedule(object : TimerTask() {
+                override fun run() {
+                    mTimerSec += 0.1
+                    mHandler.post {
+                        getContentsInfo()
+                    }
+                }
+            }, 100, 1000) // 最初に始動させるまで100ミリ秒、ループの間隔を100ミリ秒 に設定
+        }
+    }
+
+/*        // 次へボタン押下
+        prog_button.setOnClickListener {
             if (mTimer == null) {
                 mTimer = Timer()
                 mTimer!!.schedule(object : TimerTask() {
@@ -33,16 +72,52 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
-        pause_button.setOnClickListener {
+        // 戻るボタン押下
+        prev_button.setOnClickListener {
             if (mTimer != null) {
                 mTimer!!.cancel()
                 mTimer = null
             }
         }
 
-        reset_button.setOnClickListener {
+        // 再生/停止ボタン押下
+        playstop_button.setOnClickListener {
             mTimerSec = 0.0
             //timer.text = String.format("%.1f", mTimerSec)
         }
+    }
+*/
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+        when (requestCode) {
+            PERMISSIONS_REQUEST_CODE ->
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    //getContentsInfo()
+                }
+        }
+    }
+
+    // 画像の情報を取得する
+    private fun getContentsInfo() {
+        val resolver = contentResolver
+        val cursor = resolver.query(
+            MediaStore.Images.Media.EXTERNAL_CONTENT_URI, // データの種類
+            null, // 項目（null = 全項目）
+            null, // フィルタ条件（null = フィルタなし）
+            null, // フィルタ用パラメータ
+            null // ソート (nullソートなし）
+        )
+
+        if (cursor!!.moveToFirst()) {
+
+            // indexからIDを取得し、そのIDから画像のURIを取得する
+            val fieldIndex = cursor.getColumnIndex(MediaStore.Images.Media._ID)
+            val id = cursor.getLong(fieldIndex)
+            val imageUri = ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id)
+
+            imageView.setImageURI(imageUri)
+            Log.d("DEBUG_APP", "URI : " + imageUri.toString())
+            cursor.moveToNext()
+        }
+        cursor.close()
     }
 }
